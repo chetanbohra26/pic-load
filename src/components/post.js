402899bdcb42c04dc35c60a203cfd85be3c0ffed
Chetan Bohra <chetanbohra26@gmail.com>
2021-12-17 06:00:26 -0800

    fetchpost changes: UI enhancements

--- src/components/post.js
+++ src/components/post.js
@@ -1,20 +1,26 @@
 import React from "react";
+import moment from "moment";
+function Post({ post }) {
+	console.log(post);
+	const ago = moment(post.createdAt).fromNow();
 
-class Post extends React.Component {
-	render() {
-		return (
-			<div className="mb-4 border border-dark rounded d-flex flex-column w-50 align-self-center">
-				<h4 className="p-2 mb-0">{this.props.post.title}</h4>
-				<div className="d-flex justify-content-center bg-dark">
-					<img
-						src={this.props.post.postImage}
-						alt=""
-						className="mw-100"
-					/>
+	return (
+		<div className="card mb-4 border d-flex flex-column w-50 align-self-center">
+			<div className="card-header">
+				<div className="d-flex align-items-center flex-grow-1 flex-shrink-1 justify-content-between">
+					<div className="d-flex flex-row align-items-center flex-grow-1 flex-shrink-1">
+						<div className="p-4 me-2 rounded-circle bg-dark" />
+						<h3 className="p-0 m-0">{post.author}</h3>
+					</div>
+					<span>{ago}</span>
 				</div>
+				<p className="p-0 m-0 fs-5">{post.title}</p>
 			</div>
-		);
-	}
+			<div className="d-flex justify-content-center bg-dark">
+				<img src={post.postImage} alt="" className="mw-100" />
+			</div>
+		</div>
+	);
 }
 
 export default Post;
